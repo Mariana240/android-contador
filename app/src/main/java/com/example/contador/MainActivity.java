36c6192220e6bd59a798349e3f35879a089d2e3c
mariana240 <marianaramat@gmail.com>
2019-07-05 19:23:37 -0300

    modif

--- app/src/main/java/com/example/contador/MainActivity.java
+++ app/src/main/java/com/example/contador/MainActivity.java
@@ -13,6 +13,7 @@ public class MainActivity extends Activity {
 
     private int contador;
     TextView textoResultado;
+    //cristian
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
